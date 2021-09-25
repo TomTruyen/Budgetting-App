429e9df6351e5152fbd2f1e65daac3b78a94a982
TomTruyen <tom.truyen@gmail.com>
2021-09-25 13:22:49 +0200

    Add account add button

--- BudgetTracker_Remastered/app/src/main/java/com/tomtruyen/budgettracker/AccountCardAdapter.kt
+++ BudgetTracker_Remastered/app/src/main/java/com/tomtruyen/budgettracker/AccountCardAdapter.kt
@@ -11,18 +11,33 @@ import androidx.recyclerview.widget.RecyclerView
 class AccountCardAdapter(private val mContext: Context) : RecyclerView.Adapter<AccountCardAdapter.MyHolder>() {
     var mCardPageCount = 5
 
+    companion object {
+        private const val CARD = 0
+        private const val ADD_CARD = 1
+    }
+
     inner class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var mAccountName = itemView.findViewById<TextView>(R.id.account_name)
         var mAccountBalance = itemView.findViewById<TextView>(R.id.account_balance)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        if(viewType == ADD_CARD) return MyHolder(LayoutInflater.from(mContext).inflate(R.layout.card_add_fragment, parent, false))
+
         return MyHolder(LayoutInflater.from(mContext).inflate(R.layout.card_fragment, parent, false))
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if(position == mCardPageCount - 1) return ADD_CARD
+
+        return CARD
     }
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
-        holder.mAccountName.text = "Account #${position}"
+        if(holder.itemViewType == CARD) {
+            holder.mAccountName.text = "Account #${position}"
+        }
     }
 
     override fun getItemCount(): Int  = mCardPageCount
