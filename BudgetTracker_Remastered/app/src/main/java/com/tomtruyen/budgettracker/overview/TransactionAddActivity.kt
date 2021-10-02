8c3af22588f9bcba8f8f7770ff85452ab9df40a6
TomTruyen <tom.truyen@gmail.com>
2021-10-02 18:50:29 +0200

    Replace floatingactionbutton with action icon

--- BudgetTracker_Remastered/app/src/main/java/com/tomtruyen/budgettracker/overview/TransactionAddActivity.kt
+++ BudgetTracker_Remastered/app/src/main/java/com/tomtruyen/budgettracker/overview/TransactionAddActivity.kt
@@ -2,6 +2,8 @@ package com.tomtruyen.budgettracker.overview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.tomtruyen.budgettracker.R
 
 class TransactionAddActivity : AppCompatActivity() {
@@ -11,5 +13,20 @@ class TransactionAddActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.transaction_add_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+        R.id.btn_add_transaction -> {
+            finish()
+            true
+        }
+
+        else -> { super.onOptionsItemSelected(item) }
     }
 }
